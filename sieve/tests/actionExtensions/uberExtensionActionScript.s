b465bfcb5d983c12a5c695577e8dd77ecf46ba8b
James Cassell <code@james.cassell.me>
2014-11-06 00:16:58 -0500

    sieve test script

--- sieve/tests/actionExtensions/uberExtensionActionScript.s
+++ sieve/tests/actionExtensions/uberExtensionActionScript.s
@@ -1,6 +1,6 @@
 require ["reject", "fileinto", "imapflags", "vacation", "notify",
 	"vacation-seconds", "copy", "imap4flags", "relational",
-	"comparator-i;ascii-numeric"];
+	"comparator-i;ascii-numeric", "variables"];
 
 #this is for the extra thigns we have added to sieve
 
@@ -63,6 +63,23 @@ fileinto "INBOX.fileinto.internalflags";
 fileinto :flags "" "INBOX.fileinto.nullflags";
 
 }
+
+#VARIABLES
+##############################################
+if header :contains "subject" "variables"
+{
+set :lowerfirst "myvar" "myval";
+set :lower :upperfirst :quotewildcard "myvar2" "my*val2";
+
+set "mystring" "string1";
+set "mystring2" "00${mystring}00";
+
+if string "myvar2" "my*val2" {
+fileinto :copy "INBOX.stringtest.true";
+}
+
+}
+
 
 #VACATION
 #############################################
